9d1ab449179bf5e803c3e6d7e2287bd1f1713e5d
Alex Bilson <acbilson@gmail.com>
2022-12-01 13:05:32 -0700

    update ci output

--- ci-build.sh
+++ ci-build.sh
@@ -6,5 +6,5 @@ current_rev=$(git rev-parse --short HEAD)
 if [ $selected_rev = $current_rev ]; then
    podman build --target=prod -t acbilson/webhook-$current_rev:alpine -t acbilson/webhook:latest .;
 else
-   echo "the current git commit and the selected commit do not match";
+	echo "the current git commit ($current_rev) and the selected commit ($selected_rev) do not match";
 fi
