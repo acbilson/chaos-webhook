d0582dd9f511c9a54e48d5fad0fa8902d546c889
Alex Bilson <acbilson@gmail.com>
2022-12-14 10:55:26 -0700

    deserializes to json

--- tagparser_r/main.rs
+++ tagparser_r/main.rs
@@ -1,15 +1,25 @@
-use std::fs;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 use regex::Regex;
-use walkdir::WalkDir;
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::fs;
 use walkdir::DirEntry;
+use walkdir::WalkDir;
 
-fn main() {
-    get_backrefs();
+#[derive(Serialize, Deserialize)]
+struct Backref {
+    referrer: String,
+    sources: Vec<String>,
 }
 
-fn get_backrefs() {
+fn main() {
+    let source = get_backref_source();
+    let backrefs = convert_to_json(&source);
+    println!("{:?}", backrefs);
+}
+
+fn get_backref_source() -> HashMap<String, Vec<String>> {
     // [^\s]+ = puts the first word unbroken by a space into the "src" pattern
     let re = Regex::new(r"\{\{< backref.*src=(?P<src>[^\s]+) >\}\}").unwrap();
 
@@ -25,30 +35,44 @@ fn get_backrefs() {
             let file_path = entry.into_path();
             let content = fs::read_to_string(&file_path).expect("content is readable");
 
+            // generates backref source HashMap
             for backref_captures in re.captures_iter(&content) {
                 // always matches one
                 if backref_captures.len() > 1 {
-                    let reference = format!("{}.md", &backref_captures["src"].replace("\"", "").to_string());
-                    println!("path {:?}", &file_path);
-                    println!("src match {:?}", &reference);
-                    println!("");
+                    let reference = format!(
+                        "{}.md",
+                        &backref_captures["src"].replace("\"", "").to_string()
+                    );
 
                     let referrer = file_path.to_str().unwrap().to_string();
 
                     match backrefs.entry(reference) {
-                        Entry::Vacant(e) => { e.insert(vec![referrer]); },
-                        Entry::Occupied(mut e) => { e.get_mut().push(referrer); },
+                        Entry::Vacant(e) => {
+                            e.insert(vec![referrer]);
+                        }
+                        Entry::Occupied(mut e) => {
+                            e.get_mut().push(referrer);
+                        }
                     }
                 }
             }
         }
     }
+    return backrefs;
+}
 
-    for (key, value) in &backrefs {
-        println!("reference: {:?}", key);
-        for v in value {
-            println!("referrer: {:?}", v);
-        }
-        println!("");
+fn convert_to_json(source: &HashMap<String, Vec<String>>) -> String {
+    let mut backrefs: Vec<String> = Vec::new();
+    for (key, value) in source {
+        let backref = Backref {
+            referrer: key.to_string(),
+            sources: value.to_vec(),
+        };
+        let backref_str = match serde_json::to_string(&backref) {
+            Ok(b) => b,
+            Err(_) => "".to_string(),
+        };
+        backrefs.push(backref_str.to_string());
     }
+    return backrefs.join("");
 }
