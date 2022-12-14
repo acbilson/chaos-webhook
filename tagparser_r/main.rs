b0ac1c23621cee7a9a8c98038a9eb1f199ae8088
Alex Bilson <acbilson@gmail.com>
2022-12-14 14:41:30 -0700

    saves deduped tags list to output

--- tagparser_r/main.rs
+++ tagparser_r/main.rs
@@ -16,7 +16,7 @@ struct Backref {
 
 #[derive(Serialize, Deserialize, Default, Debug)]
 struct Syndicated {
-    mastodon: String
+    mastodon: String,
 }
 
 impl Syndicated {
@@ -44,7 +44,7 @@ struct FrontMatter {
     syndicated: Option<Syndicated>,
 
     #[serde(alias = "in-reply-to")]
-    inreplyto: Option<String>
+    inreplyto: Option<String>,
 }
 
 impl FrontMatter {
@@ -55,35 +55,43 @@ impl FrontMatter {
 
 impl PartialEq for FrontMatter {
     fn eq(&self, o: &Self) -> bool {
-        self.author == o.author && self.date == o.date && self.lastmod == o.lastmod && self.epistemic == o.epistemic && self.tags == o.tags && self.syndicate == o.syndicate && self.syndicated == o.syndicated
+        self.author == o.author
+            && self.date == o.date
+            && self.lastmod == o.lastmod
+            && self.epistemic == o.epistemic
+            && self.tags == o.tags
+            && self.syndicate == o.syndicate
+            && self.syndicated == o.syndicated
     }
 }
 impl Eq for FrontMatter {}
 
 #[derive(Debug)]
-    pub enum FrontMatterError {
-        MissingTomlTags,
-        NotValidToml,
-    }
+pub enum FrontMatterError {
+    MissingTomlTags,
+    NotValidToml(String),
+}
+
+struct ParseResults {
+    tagsMap: HashMap<String, Vec<String>>,
+    backrefsMap: HashMap<String, Vec<String>>
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let start_dir = &args[1];
     let out_dir = &args[2];
 
-    let content = concat!(
-    "+++\n",
-    "name = \"Alex\"\n",
-    "male = true\n",
-    "+++\n",
-    );
+    let results = parse_files(&start_dir);
 
-    let source = get_backref_source(&start_dir);
-    let backrefs = convert_to_json(&source);
-    fs::write(format!("{}/data.json", &out_dir), &backrefs).expect("writes data to data.json");
+    let backrefs = convert_to_json(&results.backrefsMap);
+    let tags = convert_to_json(&results.tagsMap);
+
+    fs::write(format!("{}/tags.json", &out_dir), &tags).expect("writes tag data to tags.json");
+    fs::write(format!("{}/backrefs.json", &out_dir), &backrefs).expect("writes backref data to backrefs.json");
 }
 
-fn get_backref_source(start_dir: &str) -> HashMap<String, Vec<String>> {
+fn parse_files(start_dir: &str) -> ParseResults {
     // [^\s]+ = puts the first word unbroken by a space into the "src" pattern
     let re = Regex::new(r"\{\{< backref.*src=(?P<src>[^\s]+) >\}\}").unwrap();
 
@@ -92,6 +100,7 @@ fn get_backref_source(start_dir: &str) -> HashMap<String, Vec<String>> {
     };
 
     let mut backrefs: HashMap<String, Vec<String>> = HashMap::new();
+    let mut tags: HashMap<String, Vec<String>> = HashMap::new();
 
     // e.ok() skips files the program can't open (insufficient permissions for example)
     for entry in WalkDir::new(start_dir).into_iter().filter_map(|e| e.ok()) {
@@ -100,12 +109,12 @@ fn get_backref_source(start_dir: &str) -> HashMap<String, Vec<String>> {
             let content = fs::read_to_string(&file_path).expect("content is readable");
             let referrer = file_path.to_str().unwrap().to_string();
 
-            let frontmatter = get_frontmatter(&content).expect("parses toml");
-            println!("{:?}", frontmatter);
+            let frontmatter = get_frontmatter(&content, &referrer).expect("parses toml");
+            add_to_tags(&referrer, &frontmatter, &mut tags);
             add_to_backrefs(&re, &referrer, &content, &mut backrefs);
         }
     }
-    return backrefs;
+    return ParseResults { tagsMap: tags, backrefsMap: backrefs };
 }
 
 fn add_to_backrefs(
@@ -138,9 +147,12 @@ fn add_to_backrefs(
 fn convert_to_json(source: &HashMap<String, Vec<String>>) -> String {
     let mut backrefs: Vec<String> = Vec::new();
     for (key, value) in source {
+        let mut deduped_sources = value.to_vec();
+        deduped_sources.sort();
+        deduped_sources.dedup();
         let backref = Backref {
             referrer: key.to_string(),
-            sources: value.to_vec(),
+            sources: deduped_sources,
         };
         let backref_str = match serde_json::to_string(&backref) {
             Ok(b) => b,
@@ -151,8 +163,28 @@ fn convert_to_json(source: &HashMap<String, Vec<String>>) -> String {
     return backrefs.join("");
 }
 
-fn get_frontmatter(content: &str) -> Result<FrontMatter, FrontMatterError> {
+fn add_to_tags(referrer: &str, frontmatter: &FrontMatter, tags: &mut HashMap<String, Vec<String>>) {
+    if let Some(current_tags) = &frontmatter.tags {
+        let mut sorted_tags = current_tags.to_vec();
+        sorted_tags.sort();
+        let mut clone = sorted_tags.clone();
 
+        for t in sorted_tags {
+            match tags.entry(String::from(&t)) {
+                Entry::Vacant(e) => {
+                    clone.retain(|x| *x != t);
+                    e.insert(clone.to_vec());
+                }
+                Entry::Occupied(mut e) => {
+                    clone.retain(|x| *x != t);
+                    e.get_mut().append(&mut clone);
+                }
+            }
+        }
+    }
+}
+
+fn get_frontmatter(content: &str, file_name: &str) -> Result<FrontMatter, FrontMatterError> {
     let parse_header = |c: &str| -> Result<String, FrontMatterError> {
         if !c.contains("+++") {
             return Err(FrontMatterError::MissingTomlTags);
@@ -163,16 +195,19 @@ fn get_frontmatter(content: &str) -> Result<FrontMatter, FrontMatterError> {
         if first_idx == last_idx {
             return Err(FrontMatterError::MissingTomlTags);
         }
-        Ok(String::from(&content[first_idx+3..last_idx]))
+        Ok(String::from(&content[first_idx + 3..last_idx]))
     };
 
     let header = parse_header(content)?;
 
-    let frontmatter: FrontMatter = toml::from_str(&header).unwrap_or(FrontMatter::new());
-
-    if frontmatter == Default::default() {
-        return Err(FrontMatterError::NotValidToml);
-    }
+    // converts toml error to my custom error type
+    let frontmatter: FrontMatter = toml::from_str(&header).map_err(|e| {
+        FrontMatterError::NotValidToml(format!(
+            "file {} parse failure: {}",
+            file_name,
+            e.to_string()
+        ))
+    })?;
 
     Ok(frontmatter)
 }
