6bd36b83dc48d033058051096537fd389a511ccd
Alex Bilson <acbilson@gmail.com>
2022-12-27 13:13:14 -0700

    repo cleanup

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. .env
-
-docker run --rm \
-  --expose ${EXPOSED_PORT} -p ${EXPOSED_PORT}:80 \
-  -v ${SOURCE_PATH}/chaos-content:/mnt/hugo/content \
-  -v ${SOURCE_PATH}/chaos-theme:/mnt/hugo/themes/chaos \
-  --name webhook \
-  acbilson/webhook-dev:alpine
--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. .env
-
-xh -v post localhost:9000/hooks/content-pull-webhook
